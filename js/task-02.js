547c6376dff585f9720e2176326e7ca5dff41a75
groguls <125222488+groguls@users.noreply.github.com>
2023-06-04 00:36:56 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+let markup = [];
+
+ingredients.forEach(item => { 
+  const itemEl = document.createElement('li');
+  itemEl.textContent = item;
+  itemEl.classList.add('item');
+  markup.push(itemEl);
+});
+
+list.append(...markup);
