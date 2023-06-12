431bcbedd28594c189537ae120460334ff6fae92
groguls <125222488+groguls@users.noreply.github.com>
2023-06-13 01:28:36 +0200

    task 08-09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
 
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event){
+    event.preventDefault();
+
+    const {elements: 
+        {email: 
+            {value: email}, 
+        password: 
+            {value: password}
+        }
+    } = event.currentTarget;
+
+    if (email === "" || password === ""){
+        return alert("Будь ласка, заповніть усі поля форми");
+    };
+
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const refs = {
+  body: document.body,
+  button: document.querySelector(".change-color"),
+  span: document.querySelector(".color"),
+};
+
+refs.button.addEventListener("click", onbuttonClick.bind(refs.span));
+
+function onbuttonClick() {
+  const randomColor = getRandomHexColor();
+  refs.body.style.backgroundColor = randomColor;
+  this.textContent = randomColor;
+};
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
